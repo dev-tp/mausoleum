d6c8aa67d83c59b76a9b7d8c115b00836e432865
Erik Sorto <rcksorto@yahoo.com>
2022-01-05 15:21:17 -0800

    Visualise state change when clicking crypt or niche

--- src/svg/template.js
+++ src/svg/template.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 import SVG from '../SVG';
 
+const colors = [
+  { fill: '#fff0', color: '#000' },
+  { fill: '#673ab7', color: '#000' },
+  { fill: '#2196f3', color: '#000' },
+  { fill: '#8bc34a', color: '#000' },
+];
+
 export default function $name() {
   const [props, setProps] = React.useState([]);
 
@@ -11,15 +18,35 @@ export default function $name() {
       .then((json) => setProps(json));
   }, [setProps]);
 
+  function push(prop) {
+    console.log(prop);
+  }
+
   function render() {
     return props.map((prop, i) => (
-      <g key={i} onClick={() => alert(i)}>
-        <path d={prop.d} />
-        <text x={prop.x} y={prop.y}>
-          {prop.spaceNumber}
+      <g key={i} onClick={() => update(prop.space_number)}>
+        <path d={prop.d} style={{ fill: colors[prop.status].fill }} />
+        <text style={{ fill: colors[prop.status].color }} x={prop.x} y={prop.y}>
+          {prop.space_number}
         </text>
       </g>
     ));
+  }
+
+  function update(spaceNumber) {
+    let modifiedProp = null;
+
+    setProps(
+      props.map((prop) => {
+        if (prop.space_number === spaceNumber) {
+          return (modifiedProp = { ...prop, status: (prop.status + 1) % 4 });
+        }
+
+        return prop;
+      })
+    );
+
+    push(modifiedProp);
   }
 
   return $body;
