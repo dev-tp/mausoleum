79d60cb1a6176fd3ce040f023c6689790155ce44
Erik Sorto <rcksorto@yahoo.com>
2022-01-07 16:24:12 -0800

    Create Python script to push data from CSV file

---
+++ src/svg/push.py
@@ -0,0 +1,37 @@
+import csv
+import mysql.connector
+
+
+def main():
+    with open('crypts_and_niches.csv') as csv_file:
+        reader = csv.reader(csv_file)
+
+        connection = mysql.connector.connect(
+            user='admin',
+            password='password',
+            host='127.0.0.1',
+            database='mausoleum'
+        )
+
+        cursor = connection.cursor()
+
+        insert_statement = (
+            'INSERT INTO ossuary '
+            '(uri, d, x, y, space_number) '
+            'VALUES (%s, %s, %s, %s, %s)'
+        )
+
+        next(reader)
+
+        for line in reader:
+            print(line)
+            cursor.execute(insert_statement, line)
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+
+if __name__ == '__main__':
+    main()
