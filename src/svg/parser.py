8b785593e6ef3e3150e260b619ca6440fe8a0779
Erik Sorto <rcksorto@yahoo.com>
2022-01-07 16:48:08 -0800

    Format Python files with `yapf`

--- src/svg/parser.py
+++ src/svg/parser.py
@@ -4,17 +4,17 @@ import subprocess
 from string import Template
 from xml.etree import ElementTree
 
-namespace = 'http://www.w3.org/2000/svg'
-prefix = '{' + namespace + '}'
+namespace = "http://www.w3.org/2000/svg"
+prefix = "{" + namespace + "}"
 
 
 def create_js_file(svg_file_name):
-    ElementTree.register_namespace('', namespace)
+    ElementTree.register_namespace("", namespace)
     tree = ElementTree.parse(svg_file_name)
 
     root = tree.getroot()
     root.attrib.clear()
-    root.tag = 'SVG'
+    root.tag = "SVG"
 
     target = None
 
@@ -25,9 +25,9 @@ def create_js_file(svg_file_name):
             root.remove(child)
 
     target.clear()
-    target.text = '{render()}'
+    target.text = "{render()}"
 
-    with open('template.js') as template_file:
+    with open("template.js") as template_file:
         template = Template(template_file.read())
 
         file_name = generate_js_file_name(svg_file_name)
@@ -38,28 +38,32 @@ def create_js_file(svg_file_name):
         except OSError:
             pass
 
-        with open(path, 'w') as javascript_file:
-            javascript_file.write(template.substitute({
-                'name': file_name,
-                'body': ElementTree.tostring(root, 'unicode', 'xml'),
-                'url': generate_uri(svg_file_name),
-            }))
+        with open(path, "w") as javascript_file:
+            javascript_file.write(
+                template.substitute({
+                    "name":
+                    file_name,
+                    "body":
+                    ElementTree.tostring(root, "unicode", "xml"),
+                    "url":
+                    generate_uri(svg_file_name),
+                }))
 
 
 def generate_js_file_name(svg_file_name):
-    tokens = svg_file_name.replace('.svg', '').split(' - ', 1)
+    tokens = svg_file_name.replace(".svg", "").split(" - ", 1)
     tokens[0] = tokens[0].title()
 
-    return ''.join(token.replace(' ', '') for token in tokens).replace('-', '')
+    return "".join(token.replace(" ", "") for token in tokens).replace("-", "")
 
 
 def generate_uri(svg_file_name):
-    tokens = svg_file_name.replace('.svg', '').split(' - ', 1)
+    tokens = svg_file_name.replace(".svg", "").split(" - ", 1)
 
     for i in range(len(tokens)):
-        tokens[i] = tokens[i].strip().replace(' ', '_').lower()
+        tokens[i] = tokens[i].strip().replace(" ", "_").lower()
 
-    return '/'.join(tokens)
+    return "/".join(tokens)
 
 
 def get_crypts_and_niches(svg_file_name):
@@ -74,10 +78,10 @@ def get_crypts_and_niches(svg_file_name):
 
     for child in target:
         if child.tag == f"{prefix}path":
-            d = child.attrib['d']
+            d = child.attrib["d"]
         elif child.tag == f"{prefix}text":
-            x = child.attrib['x']
-            y = child.attrib['y']
+            x = child.attrib["x"]
+            y = child.attrib["y"]
             text = child.text
 
         if d != None and x != None and y != None:
@@ -89,51 +93,47 @@ def get_crypts_and_niches(svg_file_name):
 
 def main():
     file_names = sorted(item for item in os.listdir()
-                        if '.svg' in item and item != 'mausoleum.svg')
+                        if ".svg" in item and item != "mausoleum.svg")
     routes = []
 
-    with open('crypts_and_niches.csv', 'w') as out:
-        out.write('location,d,x,y,space_number\n')
+    with open("crypts_and_niches.csv", "w") as out:
+        out.write("location,d,x,y,space_number\n")
 
         for file_name in file_names:
             for line in get_crypts_and_niches(file_name):
-                out.write(line + '\n')
+                out.write(line + "\n")
 
             create_js_file(file_name)
 
             routes.append({
-                'component': generate_js_file_name(file_name),
-                'label': file_name.replace('.svg', ''),
-                'path': generate_uri(file_name),
+                "component": generate_js_file_name(file_name),
+                "label": file_name.replace(".svg", ""),
+                "path": generate_uri(file_name),
             })
 
-    with open('../routes.js', 'w') as out:
+    with open("../routes.js", "w") as out:
         for route in routes:
             line = "import {0} from './components/generated/{0}';\n"
-            out.write(line.format(route['component']))
+            out.write(line.format(route["component"]))
 
-        out.write('\nconst routes = [\n')
+        out.write("\nconst routes = [\n")
 
         for route in routes:
-            out.write(
-                '  {\n'
-                f"    component: {route['component']},\n"
-                f"    label: '{route['label']}',\n"
-                f"    path: '{route['path']}',\n"
-                '  },\n'
-            )
+            out.write("  {\n"
+                      f"    component: {route['component']},\n"
+                      f"    label: '{route['label']}',\n"
+                      f"    path: '{route['path']}',\n"
+                      "  },\n")
 
-        out.write('];\n')
-        out.write('\nexport default routes;\n')
+        out.write("];\n")
+        out.write("\nexport default routes;\n")
 
     try:
-        subprocess.call(['prettier', '--write', '../components/generated'])
+        subprocess.call(["prettier", "--write", "../components/generated"])
     except FileNotFoundError:
-        print(
-            'Prettier is not installed. Run `npm i -g prettier` to format '
-            'generated JavaScript files.'
-        )
+        print("Prettier is not installed. Run `npm i -g prettier` to format "
+              "generated JavaScript files.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/svg/push.py
+++ src/svg/push.py
@@ -3,23 +3,19 @@ import mysql.connector
 
 
 def main():
-    with open('crypts_and_niches.csv') as csv_file:
+    with open("crypts_and_niches.csv") as csv_file:
         reader = csv.reader(csv_file)
 
-        connection = mysql.connector.connect(
-            user='admin',
-            password='password',
-            host='127.0.0.1',
-            database='mausoleum'
-        )
+        connection = mysql.connector.connect(user="admin",
+                                             password="password",
+                                             host="127.0.0.1",
+                                             database="mausoleum")
 
         cursor = connection.cursor()
 
-        insert_statement = (
-            'INSERT INTO ossuary '
-            '(uri, d, x, y, space_number) '
-            'VALUES (%s, %s, %s, %s, %s)'
-        )
+        insert_statement = ("INSERT INTO ossuary "
+                            "(uri, d, x, y, space_number) "
+                            "VALUES (%s, %s, %s, %s, %s)")
 
         next(reader)
 
@@ -33,5 +29,5 @@ def main():
         connection.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
