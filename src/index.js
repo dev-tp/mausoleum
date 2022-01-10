f923915ea6b84c03d03e977bea04d7e60633276a
Erik Sorto <rcksorto@yahoo.com>
2022-01-10 15:42:03 -0800

    Setup `redux`

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+import { applyMiddleware, compose, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import reducers from './reducers';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [() => () => {}];
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
