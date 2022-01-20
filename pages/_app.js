6fbf63fc389d7e3d2ec9adb307c021423e6abf85
Erik Sorto <rcksorto@yahoo.com>
2022-01-20 15:14:48 -0800

    Fade-in and fade-out between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
+import React from 'react';
+
 import '../styles/global.css';
 
+function TransitionLayout({ children }) {
+  const [currentChildren, setCurrentChildren] = React.useState(children);
+  const [transitionStage, setTransitionStage] = React.useState('fade-out');
+
+  React.useEffect(() => setTransitionStage('fade-in'), []);
+
+  React.useEffect(() => {
+    if (children !== currentChildren) {
+      setTransitionStage('fade-out');
+    }
+  }, [children, currentChildren, setCurrentChildren]);
+
+  return (
+    <div
+      className={`content ${transitionStage}`}
+      onTransitionEnd={() => {
+        if (transitionStage === 'fade-out') {
+          setCurrentChildren(children);
+          setTransitionStage('fade-in');
+        }
+      }}
+    >
+      {currentChildren}
+    </div>
+  );
+}
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <TransitionLayout>
+      <Component {...pageProps} />
+    </TransitionLayout>
+  );
 }
