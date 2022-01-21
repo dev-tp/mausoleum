6f170d36c25ea274092d0789686f46c952f422e4
Erik Sorto <rcksorto@yahoo.com>
2022-01-21 11:58:01 -0800

    Fetch location data on generated components

--- svg/template.js
+++ svg/template.js
@@ -1,9 +1,34 @@
 import dynamic from 'next/dynamic';
+import React from 'react';
 
 export default function $name() {
   const SVG = dynamic(() => import('../SVG'));
 
-  function render() {}
+  const [spaces, setSpaces] = React.useState([]);
+  const location = '$url';
+
+  React.useEffect(
+    () =>
+      fetch('/api/' + location)
+        .then((response) => response.json())
+        .then((json) => setSpaces(json)),
+    []
+  );
+
+  function render() {
+    return spaces.map((space) => (
+      <g key={space.id} onClick={() => {}}>
+        <path d={space.d} />
+        <text
+          className={space.class_name ? space.class_name : ''}
+          x={space.x}
+          y={space.y}
+        >
+          {space.space_number}
+        </text>
+      </g>
+    ));
+  }
 
   return $body;
 }
