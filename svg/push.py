53c2f8df10da1de46662e0172d8608114b7f7c08
Erik Sorto <rcksorto@yahoo.com>
2022-01-26 14:34:20 -0800

    Push CSV values to Mongo database

--- svg/push.py
+++ svg/push.py
@@ -1,32 +1,42 @@
 import csv
-import mysql.connector
+
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+
+def parse_line(location, class_name, d, x, y, space_number):
+    return {
+        'location': location,
+        'class_name': class_name,
+        'd': d,
+        'x': int(float(x)),
+        'y': int(float(y)),
+        'space_number': space_number,
+        'contract_number': '',
+        'patron': '',
+        'designee': '',
+        'date_of_birth': None,
+        'date_of_death': None,
+        'date_of_internment': None,
+        'status': 0,
+    }
 
 
 def main():
     with open('mausoleum.csv') as csv_file:
+        config = dotenv_values('../.env.local')
         reader = csv.reader(csv_file)
 
-        connection = mysql.connector.connect(user='admin',
-                                             password='password',
-                                             host='127.0.0.1',
-                                             database='mausoleum')
-
-        cursor = connection.cursor()
-
-        insert_statement = ('INSERT INTO ossuary '
-                            '(uri, d, x, y, space_number) '
-                            'VALUES (%s, %s, %s, %s, %s)')
+        client = MongoClient(config['MONGODB_URI'])
+        database = client[config['MONGODB_DB_NAME']]
 
         next(reader)
 
         for line in reader:
-            print(line)
-            cursor.execute(insert_statement, line)
+            document = parse_line(*line)
+            result = database.mausoleum.insert_one(document)
 
-        connection.commit()
-
-        cursor.close()
-        connection.close()
+            print(result.inserted_id)
 
 
 if __name__ == '__main__':
