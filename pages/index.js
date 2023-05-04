71575c8fc18c384c651bb089ced30209aa863237
Erik Sorto <rcksorto@yahoo.com>
2023-05-04 15:34:22 -0700

    Load SVG with clickable areas

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import React from 'react';
+
+import AlcoveOfTheSaintsABCD from '../svg/Alcove of the Saints - ABCD.svg';
 
 export default function Home() {
-  const Mausoleum = dynamic(() => import('../components/Mausoleum'), {
-    ssr: false,
-  });
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const paths = ref.current.querySelector('g').querySelectorAll('path');
+
+    function onClick(event) {
+      console.log(event);
+    }
+
+    paths.forEach((element) =>
+      element.addEventListener('click', onClick, false)
+    );
+
+    return () =>
+      paths.forEach((element) =>
+        element.removeEventListener('click', onClick, false)
+      );
+  }, []);
 
   return (
     <div>
       <Head>
         <title>Mausoleum</title>
       </Head>
-      <Mausoleum />
+      <AlcoveOfTheSaintsABCD ref={ref} />
     </div>
   );
 }
